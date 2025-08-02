d0badd2e3b40ca78b8c57ca98acfd3add758ae21
Bibhas Deb <69815211+imaginative-bibhas@users.noreply.github.com>
2025-08-02 21:51:42 +0530

    find duplicate from a list

---
+++ find_duplicate.py
@@ -0,0 +1,12 @@
+from typing import List
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        seen = set()
+        for num in nums:
+            if num in seen:
+                return True
+            seen.add(num)
+        return False
+
+sol=Solution()
+print(sol.containsDuplicate([1,2,3,4]))
